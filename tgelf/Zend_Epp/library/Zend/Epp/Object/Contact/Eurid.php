07b26e2871073dcf5afd18bf54123f5afaa8eac6
tgelf <tgelf@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-29 14:12:38 +0000

    Added base EURid objects (EURid extends EPP in a really ugly way)

---
+++ tgelf/Zend_Epp/library/Zend/Epp/Object/Contact/Eurid.php
@@ -0,0 +1,8 @@
+<?php
+
+class Zend_Epp_Object_Contact_Eurid extends Zend_Epp_Object_Contact
+{
+    protected $ns     = 'http://www.eurid.eu/xml/epp/contact-1.0';
+    protected $schema = 'http://www.eurid.eu/xml/epp/contact-1.0 contact-1.0.xsd';
+}
+
---
+++ tgelf/Zend_Epp/library/Zend/Epp/Object/Domain/Eurid.php
@@ -0,0 +1,8 @@
+<?php
+
+class Zend_Epp_Object_Domain_Eurid extends Zend_Epp_Object_Domain
+{
+    protected $ns     = 'http://www.eurid.eu/xml/epp/domain-1.0';
+    protected $schema = 'http://www.eurid.eu/xml/epp/domain-1.0 domain-1.0.xsd';
+}
+
